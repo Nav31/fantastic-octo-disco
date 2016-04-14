c4f18925f7d29f7c3bb421d2f9a10808e9916e95
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-04-13 21:40:23 -0400

    solves the unexpected token < issue

--- public/map.js
+++ public/map.js
@@ -1,17 +1,12 @@
 
 
+var initMap = function() {
+var map = new google.maps.Map(document.getElementById('map-canvas'), {
+	  center: {lat: 40.7073069, lng: -74.0117484},
+	  zoom: 6
+	});
+};
+$(document).ready(function() {
+  initMap();
+});
 
-// var initMap = function() {
-// 	var map = new google.maps.Map(document.getElementById('map-canvas'), {
-// 	  center: {lat: 40.7073069, lng: -74.0117484},
-// 	  zoom: 6
-// 	});
-// }
-
-
-
-// $(document).ready(function() {
-//   initMap();
-// });
-
-// console.log('hello world')
